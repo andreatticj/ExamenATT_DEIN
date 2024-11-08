1e6b922f1ee1d81b02e80661f4f7d4b21aea21e6
andreatticj <andreatticj@gmail.com>
2024-11-08 10:58:54 +0100

    CHANGE add javadoc en producto y en controler correccion erroes

--- src/main/java/eu/andreatt/examenatt_dein/controllers/ProductoController.java
+++ src/main/java/eu/andreatt/examenatt_dein/controllers/ProductoController.java
@@ -1,15 +1,28 @@
 package eu.andreatt.examenatt_dein.controllers;
 
+import eu.andreatt.examenatt_dein.dao.ProductoDao;
 import eu.andreatt.examenatt_dein.model.Producto;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class ProductoController {
 
@@ -44,7 +57,7 @@ public class ProductoController {
     private ImageView imageView;
 
     @FXML
-    private TableView<?> tableProductos;
+    private TableView<Producto> tableProductos;
 
     @FXML
     private TextField textFieldCodigoProducto;
@@ -55,34 +68,272 @@ public class ProductoController {
     @FXML
     private TextField textFieldPrecio;
 
+    private ProductoDao productoDao;
+    private Producto producto;
+
+    private ObservableList<Producto> productosExistentes;
+    private String codigo, nombre;
+    private float precio;
+    private int disponible;
+
+    String ruta, rutaImagenActual;
+
+
+    @FXML
+    public void initialize() {
+        try {
+            //Instanciar Dao
+            productoDao = new ProductoDao();
+
+            //Cargar productos
+            productosExistentes = productoDao.cargarProductos();
+
+            //Instanciar columnas
+            columnaCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+            columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+            columnaPrecio.setCellValueFactory(new PropertyValueFactory<>("precio"));
+            columnaDisponible.setCellValueFactory(cellData -> {
+                Producto p = cellData.getValue();
+                Boolean v = (p.getDisponible() == 1);
+                return new ReadOnlyBooleanWrapper(v);
+            });
+            columnaDisponible.setCellFactory(CheckBoxTableCell.<Producto>forTableColumn(columnaDisponible));
+
+            //Cargar tabla
+            tableProductos.setItems(productosExistentes);
+
+            //Menu contextual
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem imagenContextual = new MenuItem("Ver Imagen");
+            MenuItem eliminarContextual = new MenuItem("Eliminar");
+            imagenContextual.setOnAction(e -> mostrarImagen());
+            eliminarContextual.setOnAction(e -> eliminar());
+            contextMenu.getItems().add(imagenContextual);
+            contextMenu.getItems().add(eliminarContextual);
+            tableProductos.setContextMenu(contextMenu);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     void acercaDe(ActionEvent event) {
-
+        generarVentana(Alert.AlertType.INFORMATION, "Gestión de productos 1.0 \nAutor: Andrea Tortosa", "INFO");
     }
 
     @FXML
     void actionActualizar(ActionEvent event) {
+            //Validar errores
+            String errores = validarDatos();
 
-    }
+            //Mostrar errores existentes
+            if(errores.length()>0) {
+                generarVentana(Alert.AlertType.ERROR, errores, "ERROR");
+            }else {
+                //Actualizar producto - con imagen si elige
+                try {
+                    productoDao.editarProducto(new Producto(textFieldCodigoProducto.getText(), textFieldNombre.getText(), Float.parseFloat(textFieldPrecio.getText()), checkBoxDisponible.isSelected()));
+                    if (rutaImagenActual != null) {
+                        productoDao.insertarImagen(rutaImagenActual, codigo);
+                    }
+                    generarVentana(Alert.AlertType.INFORMATION, "Se ha ACTUALIZADO un producto", "INFO");
+
+                    //Refrescar tabla
+                    productosExistentes = productoDao.cargarProductos();
+                    tableProductos.setItems(productosExistentes);
+                } catch (Exception e) {
+                    generarVentana(Alert.AlertType.ERROR, e.getMessage(), "ERROR");
+                }
+                //Vaciar campos
+                vaciarCampos();
+            }
+        }
 
     @FXML
     void actionClickTabla(MouseEvent event) {
+        //Si se ha seleccionado un producto de la tabla
+        if(tableProductos.getSelectionModel().getSelectedItem()!=null) {
+            try {
+                //Cargar campos con información del producto seleccionado
+                producto = (Producto) tableProductos.getSelectionModel().getSelectedItem();
 
+                textFieldCodigoProducto.setText(producto.getCodigo());
+                textFieldNombre.setText(producto.getNombre());
+                textFieldPrecio.setText(String.valueOf(producto.getPrecio()));
+                if (producto.getDisponible()==1) {
+                    checkBoxDisponible.setSelected(true);
+                }else {
+                    checkBoxDisponible.setSelected(false);
+                }
+                //Recuperar imagen si la tiene
+                InputStream imagenAlmacenada = productoDao.dameImagen(producto.getCodigo());
+                if(imagenAlmacenada!=null) {
+                    imageView.setImage(new Image(imagenAlmacenada));
+                }else {
+                    imageView.setImage(null);
+                }
+
+                //Habilitar y Deshabilitar campos necesarios
+                textFieldCodigoProducto.setDisable(true);
+                botonCrear.setDisable(true);
+                botonActualizar.setDisable(false);
+            }catch (Exception e) {
+                generarVentana(Alert.AlertType.ERROR, e.getMessage(), "ERROR");
+            }
+        }
     }
 
     @FXML
     void actionCrear(ActionEvent event) {
+        String errores = validarDatos();
 
+        // Si hay errores en los datos, muestra una alerta con los mensajes de error.
+        if (!errores.isEmpty()) {
+            generarVentana(Alert.AlertType.ERROR, errores, "ERROR");
+        }else {
+            if(productoDao.existeProducto(codigo)) {
+                generarVentana(Alert.AlertType.ERROR, "Ya existe un producto con dicho CÓDIGO", "ERROR");
+            }else {
+                try {
+                    productoDao.crearProducto(new Producto(textFieldCodigoProducto.getText(), textFieldNombre.getText(), Float.parseFloat(textFieldPrecio.getText()), disponible));
+                    //Añadir producto - con imagen si elige
+                    if(imageView!=null) {
+                        productoDao.insertarImagen(ruta, codigo);
+                    }
+                    generarVentana(Alert.AlertType.INFORMATION, "Se ha CREADO un producto", "INFO");
+
+                    //Refrescar tabla
+                    productosExistentes = productoDao.cargarProductos();
+                    tableProductos.setItems(productosExistentes);
+                } catch (Exception e) {
+                    generarVentana(Alert.AlertType.ERROR, e.getMessage(), "ERROR");
+                }
+                //Vaciar campos
+                vaciarCampos();
+            }
+        }
     }
 
     @FXML
     void actionLimpiar(ActionEvent event) {
-
+        vaciarCampos();
     }
 
     @FXML
     void actionSeleccionarImagen(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        configureFileChooser(fileChooser);
 
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        File archivoSeleccionado = fileChooser.showOpenDialog(stage);
+
+        if (archivoSeleccionado != null) {
+            //Comprobar que la imagen no sea demasiado grande
+            try {
+                if(Files.size(archivoSeleccionado.toPath())>65000) {
+                    //Mensaje de alerta
+                    generarVentana(Alert.AlertType.ERROR, "Elige una imagen más pequeña", "ERROR");
+                }else {
+                    Image image = new Image(archivoSeleccionado.toURI().toString());
+                    ruta = archivoSeleccionado.getAbsolutePath();
+                    rutaImagenActual = ruta;
+                    imageView.setImage(image);
+                }
+            } catch (IOException e) {
+
+            }
+        }
     }
+
+    public void eliminar() {
+        try {
+            //Borrar producto
+            productoDao.borrarProducto(producto);
+            generarVentana(Alert.AlertType.INFORMATION, "Se ha BORRADO un producto", "INFO");
+            try {
+                //Refrescar tabla
+                productosExistentes = productoDao.cargarProductos();
+                tableProductos.setItems(productosExistentes);
+            } catch (Exception e) {
+                generarVentana(Alert.AlertType.ERROR, "ERROR al cargar la información", "ERROR");
+            }
+
+            //Vaciar campos
+            vaciarCampos();
+        } catch (Exception e) {
+            generarVentana(Alert.AlertType.ERROR, "ERROR al borrar producto", "ERROR");
+        }
+    }
+
+    private void vaciarCampos() {
+            textFieldCodigoProducto.setText("");
+            textFieldNombre.setText("");
+            textFieldPrecio.setText("");
+            checkBoxDisponible.setSelected(false);
+
+            textFieldCodigoProducto.setDisable(false);
+            botonCrear.setDisable(false);
+            botonActualizar.setDisable(true);
+            imageView.setImage(null);
+        }
+
+    public void mostrarImagen() {
+        // Mostrar imagen si tiene
+        if (productoDao.dameImagen(producto.getCodigo()) != null) {
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            //Instanciar layout
+            VBox vbox = new VBox();
+            vbox.setAlignment(Pos.CENTER);
+
+            //Añadir imagen
+            ImageView imageView = new ImageView(new Image(productoDao.dameImagen(producto.getCodigo())));
+            imageView.setPreserveRatio(true);
+            imageView.setFitWidth(300);
+            imageView.setFitHeight(300);
+            vbox.getChildren().add(imageView);
+
+            //Escena
+            Scene scene = new Scene(vbox, 300, 300);
+            stage.setTitle("PRODUCTO");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.showAndWait();
+        } else {
+            generarVentana(Alert.AlertType.ERROR, "El producto NO tiene imagen", "ERROR");
+        }
+    }
+
+    private void generarVentana(Alert.AlertType tipoDeAlerta, String mensaje, String title) {
+        Alert alerta = new Alert(tipoDeAlerta);
+        alerta.setContentText(mensaje);
+        alerta.setHeaderText(null);
+        alerta.setTitle(title);
+        alerta.showAndWait();
+    }
+
+    private String validarDatos() {
+        String errores = "";
+
+        // Valida que los campos no estén vacíos y que los valores numéricos sean correctos.
+        if (textFieldNombre.getText().isEmpty()) {
+            errores+=("* El nombre no puede estar vacío.\n");
+        }
+        if (textFieldCodigoProducto.getText().isEmpty()) {
+            errores+=("* El codigo del producto no puede estar vacía.\n");
+        }
+        if ( textFieldPrecio.getText().isEmpty()) {
+            errores+=("* El precio puede estar vacía.\n");
+        }
+
+        return errores;
+    }
+    private void configureFileChooser(FileChooser fileChooser) {
+        fileChooser.setTitle("Seleccionar Imágenes");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Imágenes", "*.png", "*.jpg", "*.jpeg"));
+    }
+
 
 }
--- src/main/java/eu/andreatt/examenatt_dein/model/Producto.java
+++ src/main/java/eu/andreatt/examenatt_dein/model/Producto.java
@@ -1,9 +1,11 @@
 package eu.andreatt.examenatt_dein.model;
 
 import javafx.scene.image.ImageView;
-
 import java.util.Objects;
 
+/**
+ * Clase que representa un producto en el sistema.
+ */
 public class Producto {
 
     private String codigo, nombre;
@@ -11,7 +13,14 @@ public class Producto {
     private int disponible;
     private ImageView imagen;
 
-
+    /**
+     * Constructor de la clase Producto sin imagen.
+     *
+     * @param codigo     Código del producto.
+     * @param nombre     Nombre del producto.
+     * @param precio     Precio del producto.
+     * @param disponible Cantidad disponible del producto.
+     */
     public Producto(String codigo, String nombre, float precio, int disponible) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -19,6 +28,15 @@ public class Producto {
         this.disponible = disponible;
     }
 
+    /**
+     * Constructor de la clase Producto con imagen.
+     *
+     * @param codigo     Código del producto.
+     * @param nombre     Nombre del producto.
+     * @param precio     Precio del producto.
+     * @param disponible Cantidad disponible del producto.
+     * @param imagen     Imagen asociada al producto.
+     */
     public Producto(String codigo, String nombre, float precio, int disponible, ImageView imagen) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -27,49 +45,103 @@ public class Producto {
         this.imagen = imagen;
     }
 
-
+    /**
+     * Obtiene el código del producto.
+     *
+     * @return Código del producto.
+     */
     public String getCodigo() {
         return codigo;
     }
 
+    /**
+     * Obtiene el nombre del producto.
+     *
+     * @return Nombre del producto.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del producto.
+     *
+     * @param nombre Nuevo nombre del producto.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el precio del producto.
+     *
+     * @return Precio del producto.
+     */
     public float getPrecio() {
         return precio;
     }
 
+    /**
+     * Establece el precio del producto.
+     *
+     * @param precio Nuevo precio del producto.
+     */
     public void setPrecio(float precio) {
         this.precio = precio;
     }
 
+    /**
+     * Obtiene la cantidad disponible del producto.
+     *
+     * @return Cantidad disponible del producto.
+     */
     public int getDisponible() {
         return disponible;
     }
 
+    /**
+     * Establece la cantidad disponible del producto.
+     *
+     * @param disponible Nueva cantidad disponible del producto.
+     */
     public void setDisponible(int disponible) {
         this.disponible = disponible;
     }
 
+    /**
+     * Obtiene la imagen asociada al producto.
+     *
+     * @return Imagen del producto.
+     */
     public ImageView getImagen() {
         return imagen;
     }
 
+    /**
+     * Establece la imagen asociada al producto.
+     *
+     * @param imagen Nueva imagen del producto.
+     */
     public void setImagen(ImageView imagen) {
         this.imagen = imagen;
     }
 
-
+    /**
+     * Calcula el valor de hash del objeto Producto.
+     *
+     * @return Valor de hash basado en los atributos del producto.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(codigo, disponible, nombre, precio);
     }
 
+    /**
+     * Compara este producto con otro para determinar si son iguales.
+     *
+     * @param obj Objeto a comparar con este producto.
+     * @return true si los productos son iguales; false en caso contrario.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
